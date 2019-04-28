21082f863fef8a69ed1224acee76917d0728226a
zhangmneg <1120950458@qq.com>
2019-04-28 18:16:51 +0800

    测绘4班   张孟  第7章作业

---
+++ 作业/7-11/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{//a
+    array<int,10>counts;
+    for(size_t i=0; i<counts.size();++i)
+    counts[i]=0;
+ //b
+ array<int,15>counts;
+    for(size_t j=0;j<bonus.size();++j)
+//c
+array<double,12>monthlyTemperatuers;
+for(size_t x=0;x<monthlyTemperatuers.size();++x)
+{
+    for(unsigned double number=0;number<monthlyTemperatuers.[x];++number)
+    cout<<x;
+
+}
+//d
+count<<"element"<<setw(13)<<"value"<<endl;
+
+array<int,5>bestscors;
+for(size_t c=0;c<bestscors.size();++c)
+    cout<<setw(7)<<c<<setw<<13<<n[c]<<endl;
+
+
+
+
+
+
+}
